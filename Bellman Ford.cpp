57ed87175d6b5ede1f90f7af3535402e9eb2c247
abhishekdual1212 <121820310+abhishekdual1212@users.noreply.github.com>
2025-10-30 11:08:00 +0530

    Create  Bellman Ford.cpp
    Problem statement
    You have been given a directed weighted graph of ‘N’ vertices labeled from 1 to 'N' and ‘M’ edges. Each edge connecting two nodes 'u' and 'v' has a weight 'w' denoting the distance between them.
    
    
    
    Your task is to calculate the shortest distance of all vertices from the source vertex 'src'.
    
    
    
    Note:
    If there is no path between 'src' and 'ith' vertex, the value at 'ith' index in the answer array will be 10^8.
    Example :
    
    3 3 1
    1 2 2
    1 3 2
    2 3 -1
    
    In the above graph: 
    
    The length of the shortest path between vertex 1 and vertex 1 is 1->1 and the cost is 0.
    
    The length of the shortest path between vertex 1 and vertex 2 is 1->2 and the cost is 2.
    
    The length of the shortest path between vertex 1 and vertex 3 is 1->2->3 and the cost is 1.
    
    Hence we return [0, 2, 1].
    Note :
    
    It's guaranteed that the graph doesn't contain self-loops and multiple edges. Also, the graph does not contain negative weight cycles.
    Detailed explanation ( Input/output format, Notes, Images )
    Sample Input 1 :
    4 4 1
    1 2 4
    1 3 3
    2 4 7 
    3 4 -2
    Sample Output 1 :
    0 4 3 1
    Explanation For Sample Output 1 :
    
    In the above graph: 
    
    The length of the shortest path between vertex 1 and vertex 1 is 1->1 and the cost is 0.
    
    The length of the shortest path between vertex 1 and vertex 2 is 1->2 and the cost is 4.
    
    The length of the shortest path between vertex 1 and vertex 3 is 1->3 and the cost is 3.
    
    The length of the shortest path between vertex 1 and vertex 4 is 1->3->4 and the cost is 1.
    
    Hence we return [0, 4, 3, 1].
    Sample Input 2 :
    2 1 1
    2 1 3
    Sample Output 2 :
    0 1000000000
    Constraints :
    1 <= N <= 50
    1 <= M <= 300
    1 <= src <= N
    1 <= u,v <= N
    -10^5 <= w <= 10^5
    
    Time Limit: 1 sec

---
+++ Bellman Ford.cpp
@@ -0,0 +1,15 @@
+vector<int> bellmonFord(int n, int m, int src, vector<vector<int>> &edges) {
+    dist[src]=0;
+    for(int i=1;i<n;i++){
+        for(int j=0;j<m;j++){
+            int u=edges[j][0];
+            int v=edges[j][1];
+            int wt=edges[j][2];
+            if((dist[u]+wt)<dist[v]){
+                dist[v]=dist[u]+wt;
+            }
+        }
+    }
+    return dist;
+
+}
